a52e05c028f23569131363a684482d817a91674f
Andy <shena@bu.edu>
2015-01-24 15:46:18 -0500

    minigame and asteroid script

---
+++ Assets/AsteroidController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidController : MonoBehaviour {
+
+	public float speed;
+	public float startsize;
+	public float endsize;
+	public float beat = 0.046875f;
+	public float startposition;
+	public float endposition; 
+	public int rhythmIndex; 
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	// Update is called once per frame
+	void Update () {
+
+		transform.localScale = Vector3.one * (Vector3.Distance (transform.position, transform.parent.position)
+		/ Vector3.Distance (transform.position, camera.main.transform.position));
+		//start position: 	2.6, 1.7, -1.6
+		//end position: -0.3, -1, -1.6
+		//start scale: 0.5,0.5,0.5
+		//end scale: 8, 8, 8 
+
+		Vector3.MoveTowards(transform.position, endposition, rhythmindex*beat; 
+		startposition = transform.localPosition.x ;
+		if (startposition <= endposition) 
+		{
+			destroy(); 
+		}
+		transform.Translate (0, 0, -rhythmIndex*speed);
+	}
+
+	void destroy() {
+		Destroy (transform.gameObject);
+		//start destroy animation
+		//remove itself
+	}
+}
---
+++ Assets/Minigame.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minigame : MonoBehaviour {
+	private int[] rhythm;
+	private AsteroidController newAsteroid;
+	private float endPosition = -12;
+
+	void Spawn(Vector3 position, float endposition) {
+		GameObject asteroid = GameObject.Instantiate (Resources.Load("Prefabs/Asteroid")) as GameObject;
+		newAsteroid = asteroid.GetComponent<AsteroidController> ();
+		newAsteroid.transform.position = position;
+		newAsteroid.endposition = endposition;
+	}
+
+	// Use this for initialization
+	void Start () {
+		rhythm = new int[]{1, 0, 1, 0};
+		int length = rhythm.Length;
+		Vector3 spawnOffset = new Vector3(-0.3f, -1f, -0.5f);
+		for(int i=0; i < length; i++) {
+			if (rhythm[i] == 1) {
+				Vector3 spawnPosition = transform.position + spawnOffset;
+				spawnPosition.z = spawnPosition.z + i * (spawnPosition.z - endPosition);
+				Spawn(spawnPosition, transform.position.z + endPosition);
+			}
+		}
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
