dcee822d6b8fbb1a5462944c6e1c557a866d9dde
blefevre <blefevre7@gmail.com>
2015-01-24 15:17:30 -0500

    GamePanel Class

--- Assets/Scripts/GamePanel.cs
+++ Assets/Scripts/GamePanel.cs
@@ -4,22 +4,48 @@ using System.Collections;
 public class GamePanel : MonoBehaviour {
 
 	public Minigame[] minigameArray;
-
+	public GameObject tempMiniGame;
+	public float panelZDistance;
+	private Vector3[] miniGamePositions;
 	// Use this for initialization
 	void Start () {
+
+		//Find Centre points of Quadrants;
+		miniGamePositions = new Vector3[4];
+		miniGamePositions[0] = Camera.main.ScreenToWorldPoint(new Vector3((Screen.width*0.25f), (Screen.height*0.25f), Camera.main.nearClipPlane+panelZDistance));//new Vector3((Screen.width*0.25f),transform.y,(Screen.height*0.25f));
+		miniGamePositions[1] = Camera.main.ScreenToWorldPoint(new Vector3((Screen.width*0.25f)*3, (Screen.height*0.25f), Camera.main.nearClipPlane+panelZDistance));//new Vector3((Screen.width*0.25f),transform.y,(Screen.height*0.25f));
+		miniGamePositions[2] = Camera.main.ScreenToWorldPoint(new Vector3((Screen.width*0.25f)*3, (Screen.height*0.25f)*3, Camera.main.nearClipPlane+panelZDistance));//new Vector3((Screen.width*0.25f),transform.y,(Screen.height*0.25f));
+		miniGamePositions[3] = Camera.main.ScreenToWorldPoint(new Vector3((Screen.width*0.25f), (Screen.height*0.25f)*3, Camera.main.nearClipPlane+panelZDistance));//new Vector3((Screen.width*0.25f),transform.y,(Screen.height*0.25f));
+
 		//Create Minigames
-		for(int i = 0; i < 4; i++)
-			minigameArray[i] = new Minigame();
+		minigameArray = new Minigame[4];
+		for(int i = 0; i < 4; i++){
+			createMinigame(i);
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	foreach(Vector3 v in miniGamePositions)
+			Debug.DrawLine(v, v+Vector3.forward);
+	}
+
+	private void DestroyMinigame(int quad)
+	{
+		DestroyImmediate(minigameArray[quad].transform);
+	}
+
+	private void createMinigame(int quad)
+	{	
+		//GameObject newGame = //Score.GetNewMiniGame();
+		GameObject newGO = Instantiate(tempMiniGame, miniGamePositions[quad],tempMiniGame.transform.rotation) as GameObject;
+		newGO.renderer.material.color = new Color(Random.value,Random.value,Random.value);
+		newGO.transform.parent = transform;
+		minigameArray[quad] = newGO.GetComponent<Minigame>() as Minigame;
+		minigameArray[quad].quad = quad;
+		minigameArray[quad].width = Screen.width*0.5f;
+		minigameArray[quad].height = Screen.height*0.5f;
 	}
 }
 
-public class Minigame
-{
 
-}
---
+++ Assets/Scripts/Minigame.cs
@@ -0,0 +1,82 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class Minigame : MonoBehaviour {
+
+	public int quad;
+	public float width, height;
+
+	void Start()
+	{	
+		float panelZDistance = transform.parent.GetComponent<GamePanel>().panelZDistance;
+		Vector3 point1 = Camera.main.ScreenToWorldPoint(new Vector3(0,0,Camera.main.nearClipPlane + panelZDistance));
+		Vector3 point2 = Camera.main.ScreenToWorldPoint(new Vector3(width,height,Camera.main.nearClipPlane + panelZDistance));
+		Vector2 dimensions = new Vector2(point1.x -point2.x,point1.y-point2.y)*0.5f;
+		
+		//Create Background Plane
+		Mesh m = new Mesh();
+		m.name = "Minigame " + quad;
+		m.vertices = new Vector3[] {
+			new Vector3(-dimensions.x,0,-dimensions.y),
+			new Vector3(-dimensions.x,0,dimensions.y),
+			new Vector3(dimensions.x,0,dimensions.y),
+			new Vector3(dimensions.x,0,-dimensions.y)
+		};
+		m.uv = new Vector2[] {
+		new Vector2 (0, 0),
+			new Vector2 (0, 1),
+			new Vector2(1, 1),
+			new Vector2 (1, 0)
+		};
+		m.triangles = new int[] { 0, 1, 2, 0, 2, 3};
+		m.RecalculateNormals();
+
+		GetComponent<MeshFilter>().mesh = m;
+	}
+
+	void OnEnable()
+	{
+	    // subscribe to gesture's Pan event
+	    GetComponent<TapHandler>().TapAction += TappedTest;
+	    GetComponent<SwipeHandler>().UpSwipeAction += UpSwipeTest;
+	    GetComponent<SwipeHandler>().DownSwipeAction += DownSwipeTest;
+	    GetComponent<SwipeHandler>().LeftSwipeAction += LeftSwipeTest;
+	    GetComponent<SwipeHandler>().RightSwipeAction += RightSwipeTest;
+	}
+
+	void OnDisable()
+	{
+	    // subscribe to gesture's Pan event
+	    GetComponent<TapHandler>().TapAction -= TappedTest;
+	    GetComponent<SwipeHandler>().UpSwipeAction -= UpSwipeTest;
+	    GetComponent<SwipeHandler>().DownSwipeAction -= DownSwipeTest;
+	    GetComponent<SwipeHandler>().LeftSwipeAction -= LeftSwipeTest;
+	    GetComponent<SwipeHandler>().RightSwipeAction -= RightSwipeTest;
+	}
+
+	private void TappedTest()
+	{
+		Debug.Log("TAPPED TEST EVENT");
+	}
+
+	private void DownSwipeTest()
+	{
+		Debug.Log("DOWN SWIPE TEST EVENT");
+	}
+
+	private void UpSwipeTest()
+	{
+		Debug.Log("UP SWIPE TEST EVENT");
+	}
+
+	private void LeftSwipeTest()
+	{
+		Debug.Log("LEFT SWIPE TEST EVENT");
+	}
+
+	private void RightSwipeTest()
+	{
+		Debug.Log("RIGHT SWIPE TEST EVENT");
+	}
+}
