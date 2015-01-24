cc53408d61d44e4ec3ef422fa04791b9988d9d51
Andy <shena@bu.edu>
2015-01-24 17:17:35 -0500

    moved all asteroid related code to asteroid scripts

---
+++ Assets/Scripts/AsteroidScripts/AsteroidControl.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidControl : MonoBehaviour {
+
+	public int quad;
+	public float width, height;
+	
+	public Transform foreground, background;
+	private int[] rhythm;
+	private AsteroidController newAsteroid;
+	private float endPosition = -12;
+	
+	void Spawn(Vector3 position, float endposition, int rhythmIndex) {
+		GameObject asteroid = GameObject.Instantiate (Resources.Load("Prefabs/Asteroid")) as GameObject;
+		newAsteroid = asteroid.GetComponent<AsteroidController> ();
+		newAsteroid.transform.position = position;
+		newAsteroid.endposition = endposition;
+		newAsteroid.rhythmIndex = rhythmIndex; 
+	}
+	
+	// Use this for initialization
+	void Start () {
+		rhythm = new int[]{1, 0, 1, 0};
+		int length = rhythm.Length;
+		Vector3 spawnOffset = new Vector3(-0.3f, -1f, -0.5f);
+		for(int rhythmIndex=0; rhythmIndex < length; rhythmIndex++) {
+			if (rhythm[rhythmIndex] == 1) {
+				Vector3 spawnPosition = transform.position + spawnOffset;
+				spawnPosition.z = spawnPosition.z + rhythmIndex * (spawnPosition.z - endPosition);
+				//Spawn(spawnPosition, transform.position.z + endPosition, rhythmIndex);
+			}
+		}
+		Debug.Log("test");
+		//Move foreground and background
+		if (foreground)
+			foreground.position = new Vector3(foreground.position.x,foreground.position.y,Camera.main.nearClipPlane);
+		
+		if (background)
+			background.position = new Vector3(background.position.x,background.position.y,Camera.main.farClipPlane-100);
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+}
---
+++ Assets/Scripts/AsteroidScripts/AsteroidMover.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AstroidMover : MonoBehaviour {
+
+	private float speed = 0.1875f;
+	public float startsize;
+	public float endsize;
+	public float beat = 0.046875f;
+	public float startposition;
+	public float endposition; 
+	public int rhythmIndex; 
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	// Update is called once per frame
+	void Update() {
+		transform.localScale = Vector3.one * (Vector3.Distance (transform.position, transform.parent.position)
+		                                      / Vector3.Distance (transform.position, Camera.main.transform.position));
+		//start position: 	2.6, 1.7, -1.6
+		//end position: -0.3, -1, -1.6
+		//start scale: 0.5,0.5,0.5
+		//end scale: 8, 8, 8 
+		
+		Vector3.MoveTowards(transform.position, transform.position + transform.forward * endposition, rhythmIndex*beat); 
+		startposition = transform.localPosition.x ;
+		if (startposition <= endposition) 
+		{
+			destroy(); 
+		}
+		transform.Translate (0, 0, -1);
+	}
+	
+	void destroy() {
+		Destroy (transform.gameObject);
+		//start destroy animation
+		//remove itself
+	}
+}
